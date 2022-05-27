dbea1ad3762714908a3b18b6102a1e55920fe9cb
Kevin Genchev <kevin_genchev@epam.com>
2022-05-27 09:51:26 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import PropTypes from "prop-types";
+
+function Header({ text, bgColor, textColor }) {
+  const headerStyle = {
+    backgroundColor: bgColor,
+    color: textColor,
+  };
+  return (
+    <header style={headerStyle}>
+      <div className="container">
+        <h2>{text}</h2>
+      </div>
+    </header>
+  );
+}
+
+Header.defaultProps = {
+  text: "Feedback UI",
+  bgColor: "red",
+  textColor: "blue",
+};
+
+export default Header;
